345dffa8815c1407bed177278b1c95594304dfb4
LeventLei <443706199@qq.com>
2020-03-17 18:07:56 +0800

    init

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+// import LinkList from './LinkList'
+import CreateLink from './CreateLink'
+class App extends Component {
+	render() {
+		// return <LinkList />
+		return <CreateLink />
+	}
+}
+
+export default App
